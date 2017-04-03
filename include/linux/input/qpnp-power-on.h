3be497b78e1f6322f5090cb970743891cd43eb9f
Humberto Borba <humberos@gmail.com>
2017-04-03 19:04:36 -0300

    drivers: power: reset: Fix adb reboot bootloader/recovery
    This change is based on OnePlus3 kernel which made
    a hack to force a soft reset as this preserves
    memory contents, including pstore and other
    persistent memory.
    
    In this case we are focing the soft reset only if the bootloader or
    recovery is sent as cmd.
    
    Reference: https://github.com/LineageOS/android_kernel_oneplus_msm8996/commit/f0979313e42697b3bc2311f0241cfb045c18eed0
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ibf201de9cdceab8434926d9e35e453c526c14056
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- include/linux/input/qpnp-power-on.h
+++ include/linux/input/qpnp-power-on.h
@@ -61,6 +61,7 @@ enum pon_restart_reason {
 	PON_RESTART_REASON_KEYS_CLEAR		= 0x06,
 
 	/* 32 ~ 63 for OEMs/ODMs secific features */
+	PON_RESTART_REASON_REBOOT               = 0x10,
 	PON_RESTART_REASON_OEM_MIN		= 0x20,
 	PON_RESTART_REASON_OEM_MAX		= 0x3f,
 };
