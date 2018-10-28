8ca703dedc6147d5c67e06d6f7ecb44e31615836
Artem Borisov <dedsa2002@gmail.com>
2018-10-28 18:53:50 +0400

    drivers: input: fingerprint: ysl: goodix: Convert to wakeup sources
    Change-Id: I7cd8b19f87681ae7acc264a73659752bc040dcfc

--- drivers/input/fingerprint/ysl/goodix/gf_spi.c
+++ drivers/input/fingerprint/ysl/goodix/gf_spi.c
@@ -42,7 +42,6 @@
 #include <linux/pm_qos.h>
 #include <linux/cpufreq.h>
 #include <linux/mdss_io_util.h>
-#include <linux/wakelock.h>
 #include <linux/proc_fs.h>
 #include "gf_spi.h"
 #include <linux/unistd.h>
@@ -79,7 +78,7 @@ static int SPIDEV_MAJOR;
 static DECLARE_BITMAP(minors, N_SPI_MINORS);
 static LIST_HEAD(device_list);
 static DEFINE_MUTEX(device_list_lock);
-static struct wake_lock fp_wakelock;
+static struct wakeup_source fp_wakelock;
 static struct gf_dev gf;
 
 extern int fpsensor;
@@ -349,7 +348,7 @@ static irqreturn_t gf_irq(int irq, void *handle)
 #if defined(GF_NETLINK_ENABLE)
 	struct gf_dev *gf_dev = &gf;
 	char msg = GF_NET_EVENT_IRQ;
-	wake_lock_timeout(&fp_wakelock, msecs_to_jiffies(WAKELOCK_HOLD_TIME));
+	__pm_wakeup_event(&fp_wakelock, msecs_to_jiffies(WAKELOCK_HOLD_TIME));
 	sendnlmsg(&msg);
 	if (gf_dev->device_available == 1) {
 		gf_dev->wait_finger_down = false;
@@ -828,7 +827,7 @@ static int gf_probe(struct platform_device *pdev)
 
 
 
-	wake_lock_init(&fp_wakelock, WAKE_LOCK_SUSPEND, "fp_wakelock");
+	wakeup_source_init(&fp_wakelock, "fp_wakelock");
 
 	proc_entry = proc_create(PROC_NAME, 0777, NULL, &proc_file_ops);
 	 if (NULL == proc_entry)
@@ -878,7 +877,7 @@ static int gf_remove(struct platform_device *pdev)
 {
 	struct gf_dev *gf_dev = &gf;
 
-	wake_lock_destroy(&fp_wakelock);
+	wakeup_source_trash(&fp_wakelock);
 	fb_unregister_client(&gf_dev->notifier);
 	if (gf_dev->input)
 		input_unregister_device(gf_dev->input);
