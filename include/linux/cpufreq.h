a4a51efb9919f71cb6e74f6427d5591e07ceb432
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-03-13 10:47:12 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I50d41aa3338803cbd45ff6314b2bb3978c59282b
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -946,6 +946,7 @@ int cpufreq_generic_init(struct cpufreq_policy *policy,
 		struct cpufreq_frequency_table *table,
 		unsigned int transition_latency);
 
+extern unsigned int cpuinfo_max_freq_cached;
 struct sched_domain;
 unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 unsigned long cpufreq_scale_max_freq_capacity(struct sched_domain *sd, int cpu);
