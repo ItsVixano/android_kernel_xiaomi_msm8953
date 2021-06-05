e8af79dd6a231069b874df154bf827bd17d85755
ItsVixano <vixanonew@gmail.com>
2021-06-05 21:56:15 +0200

    drivers: input: touchscreen: sakura: Shut up!
    Change-Id: I9f689476fc262c10f44c5fef388622dba8fcc7c1

--- drivers/input/touchscreen/ft5446_sakura/focaltech_config.h
+++ drivers/input/touchscreen/ft5446_sakura/focaltech_config.h
@@ -100,7 +100,7 @@
  * show debug log info
  * enable it for debug, disable it for release
  */
-#define FTS_DEBUG_EN                            1
+#define FTS_DEBUG_EN                            0
 
 /*
  * Linux MultiTouch Protocol
@@ -112,7 +112,7 @@
  * Report Pressure in multitouch
  * 1:enable(default),0:disable
 */
-#define FTS_REPORT_PRESSURE_EN                  1
+#define FTS_REPORT_PRESSURE_EN                  0
 
 /*
  * Gesture function enable
@@ -130,7 +130,7 @@
  * Production test enable
  * 1: enable, 0:disable(default)
  */
-#define FTS_TEST_EN                             1
+#define FTS_TEST_EN                             0
 
 /*
  * Glove mode enable
