fcbfab5399f0f61d2965b6f9f923edfc51078671
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-20 03:08:34 +0200

    drivers: leds: Import Xiaomi changes
    Change-Id: I5a12531cf33b9c407bcc57d567709ca2ffd25fd8

--- drivers/leds/leds-qpnp-wled.c
+++ drivers/leds/leds-qpnp-wled.c
@@ -443,6 +443,10 @@ module_param_named(
 	step_delay_gain, qpnp_wled_step_delay_gain, int, 0600
 );
 
+#ifdef CONFIG_MACH_XIAOMI_VINCE
+static int first_set_prev_state = 0;
+#endif
+
 /* helper to read a pmic register */
 static int qpnp_wled_read_reg(struct qpnp_wled *wled, u16 addr, u8 *data)
 {
@@ -1143,6 +1147,13 @@ static void qpnp_wled_work(struct work_struct *work)
 			goto unlock_mutex;
 		}
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_VINCE
+	if (1 == first_set_prev_state) {
+		wled->prev_state = true;
+		first_set_prev_state = 0;
+	}
+#endif
 
 	if (!!level != wled->prev_state) {
 		if (!!level) {
@@ -2758,6 +2769,11 @@ static int qpnp_wled_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "wled config failed\n");
 		return rc;
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_VINCE
+	if (strnstr(saved_command_line, "androidboot.mode=ffbm-01", strlen(saved_command_line)))
+		first_set_prev_state = 1;
+#endif
 
 	INIT_WORK(&wled->work, qpnp_wled_work);
 	wled->ramp_ms = QPNP_WLED_RAMP_DLY_MS;
